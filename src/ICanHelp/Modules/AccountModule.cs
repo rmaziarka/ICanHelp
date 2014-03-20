4c7af1872fe4ded4ffa4d071f158d2882f0150b9
maziarka.radoslaw@outlook.com <maziarka.radoslaw@outlook.com>
2014-03-20 20:58:56 +0100

    First commit

---
+++ src/ICanHelp/Modules/AccountModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace ICanHelp.Modules
+{
+    public class AccountModule:NancyModule
+    {
+        public AccountModule() : base("api/account")
+        {
+            Get["zaloguj"] = _ => View["Login"];
+        }
+    }
+}
---
+++ src/ICanHelp/Modules/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Simple.Data;
+
+namespace ICanHelp.Modules
+{
+    public class HomeModule:NancyModule
+    {
+
+        public HomeModule()
+        {
+
+            Get["/"] = _ => View["Index"];
+        }
+    }
+}
